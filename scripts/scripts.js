faf8021e14ea86c150ce816ac2b7d0e699bd2477
Vinylteeth <190985880+Vinylteeth@users.noreply.github.com>
2024-12-28 23:37:32 -0500

    Removes grid layout, added tabs, designing layout

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,82 +4,46 @@
 //........... 1.2 Const
 // if (navigator.clipboard.readText() == "Copy That") {this.alert("MSE");}
 
-//........... 3.1 Theme
-// Function for changing the theme between drk (default) and lght. Temporarily set onclick to corner div.
-document.getElementById("divTheme").addEventListener("click", fxThemeChange); // When theme button is clicked.
-function fxThemeChange() {
-  let elements = document.getElementsByClassName("drk-thm"); // Find all the theme elements.
-  Array.prototype.forEach.call(elements, function (element) {
-    // Loop through the theme elements with forEach.
-    element.classList.toggle("lght-thm"); // Change theme elements to light or dark. // Not sure how this gets back to dark theme but it does.
-  });
-  let thmText = document.getElementById("divTheme").innerHTML;
-  if (thmText == "D") {
-    document.getElementById("divTheme").innerHTML = "L";
-  } else {
-    document.getElementById("divTheme").innerHTML = "D";
-  }
-}
-
 //......... 8. Test Code
 
-// Fullscreen button
-window.addEventListener("click", function (event) {
-  let workingID = event.target.id;
-  let neededID = "divFullscreen";
-  if (workingID == neededID) {
-    if (!document.fullscreenElement) {
-      document.documentElement.requestFullscreen();
-    } else if (document.exitFullscreen) {
-      document.exitFullscreen();
-    }
-  }
-});
+function openAsset(evt, assetName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
 
-window.addEventListener("keydown", function (event) {
-  if (event.key == "Home") {
-    window.location.href = "http://localhost:3000/settings.html";
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
-});
 
-window.addEventListener("click", function (event) {
-  if (event.target == document.getElementById("bridgePendingID")) {
-    var getInput = "bridgePendingID";
-    localStorage.setItem("storageName", getInput);
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-});
 
-window.addEventListener("click", function (event) {
-  if (event.target == document.getElementById("bridgeInProgressID")) {
-    var getInput = "bridgeInProgressID";
-    localStorage.setItem("storageName", getInput);
-  }
-});
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(assetName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
 
-window.addEventListener("click", function (event) {
-  if (event.target == document.getElementById("bridgeSubmittedID")) {
-    var getInput = "bridgeSubmittedID";
-    localStorage.setItem("storageName", getInput);
-  }
-});
+function openBridgeComponent(evt, componentName) {
+  // Declare all variables
+  var i, tabcontentNested, tablinksNested;
 
-window.addEventListener("click", function (event) {
-  if (event.target == document.getElementById("totalPendingID")) {
-    var getInput = "totalPendingID";
-    localStorage.setItem("storageName", getInput);
+  // Get all elements with class="tabcontentNested" and hide them
+  tabcontentNested = document.getElementsByClassName("tabcontentNested");
+  for (i = 0; i < tabcontentNested.length; i++) {
+    tabcontentNested[i].style.display = "none";
   }
-});
 
-window.addEventListener("click", function (event) {
-  if (event.target == document.getElementById("totalInProgressID")) {
-    var getInput = "totalInProgressID";
-    localStorage.setItem("storageName", getInput);
+  // Get all elements with class="tablinksNested" and remove the class "active"
+  tablinksNested = document.getElementsByClassName("tablinksNested");
+  for (i = 0; i < tablinksNested.length; i++) {
+    tablinksNested[i].className = tablinksNested[i].className.replace(" active", "");
   }
-});
 
-window.addEventListener("click", function (event) {
-  if (event.target == document.getElementById("totalSubmittedID")) {
-    var getInput = "totalSubmittedID";
-    localStorage.setItem("storageName", getInput);
-  }
-});
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(componentName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
